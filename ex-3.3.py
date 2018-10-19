5ea42251a68e408acbfb561bf5df00f6a0a33701
Rita2847252519 <43162604+Rita2847252519@users.noreply.github.com>
2018-10-19 11:55:14 +0800

    Update ex-3.3.py
    10day

--- ex-3.3.py
+++ ex-3.3.py
@@ -3,9 +3,4 @@ for i in range(1,366):
     if i % 10 in [4,5,6,7]:
         dayup=dayup*(1+dayfactor)
     print("{}：天的能力{}".format(i+1,dayup))
-print("连续学习3天能力值不变，从第4天至第7天每天能力增长为前一天1%，每10天休息一天的能力值:{:.2f}.".format(dayup))
-elif i % 15 in [4,5,6,7]:
-         dayup=dayup*(1+dayfactor)
-    print("{}：天的能力{}".format(i+1,dayup))
-print("连续学习3天能力值不变，从第4天至第7天每天能力增长为前一天1%，每15天休息一天的能力值 {:.2f}.".format(dayup))       
-
+print("连续学习3天能力值不变，从第4天至第7天每天能力增长为前一天1%，每10天休息一天的能力值:{:.2f}.".format(dayup))   
